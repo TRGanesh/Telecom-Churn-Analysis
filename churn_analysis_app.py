e2f030f6395f4b504351af7e95e3c906b1bb202d
TRGanesh <117368449+TRGanesh@users.noreply.github.com>
2023-04-12 00:36:57 +0530

    Update churn_analysis_app.py

--- churn_analysis_app.py
+++ churn_analysis_app.py
@@ -87,7 +87,7 @@ def main():
                 subheader('What is Churn Analysis?')
                 st.write("""Customer churn analysis is the process of using the churn data to understand behavior of customers in a business.We can know some of the reasons about why a customer is leaving.For better growth in a business,business owners should understand the major characteristics or facilities that they can provide to their customers.""")
                 st.write("""Customer Churn is one of the problems for businesses such as Credit Card companies, cable service providers, SASS and telecommunication companies worldwide. Even though it is not the most fun to look at, customer churn metrics can help businesses improve customer retention.""")
-                st.write("""Churn analysis is the evaluation of a company’s customer loss rate in order to reduce it. Also referred to as **Customer Attrition Rate**, churn can be minimized by assessing a business product and how people use it.""")
+                st.write("""Churn analysis is the evaluation of a company’s customer loss rate in order to reduce it. Also referred to as **:red[Customer Attrition Rate]**, churn can be minimized by assessing a business product and how people use it.""")
             with image_column:
                 #st_lottie(lottie_coding1,height=300,key='lottie_coding1') 
                 st.image(Image.open('churn_customers_image.png'),width=450)
